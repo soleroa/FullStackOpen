be534c6544384a0811c9af17a68bddb473f33855
sole <msoleroa@gmail.com>
2024-12-18 20:07:23 -0300

    comienzo de la app

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,36 @@
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <p>Hello world</p>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+const Header = (props) =>{
+  <div>
+    <h1>{props.course}</h1>
+  </div>
+}
+
+const Content = () =>{
+  
+}
+export default App
