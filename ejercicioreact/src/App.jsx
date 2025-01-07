657a81a4afcc2789588b2c7a05482ce9208c09ee
Soledad Roa <74988785+soleroa@users.noreply.github.com>
2025-01-07 16:46:36 +0000

    ejercicios de prueba

---
+++ ejercicioreact/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+
+      <button onClick={() => setCounter(0)}>zero</button>
+    </div>
+  );
+};
